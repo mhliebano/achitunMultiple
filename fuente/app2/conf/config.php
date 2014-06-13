f4c4589b12bc9147535414c84bffe2236f6d3b1d
miguel <miguel@Pc1.cantv.net>
2014-06-12 21:39:35 -0430

    archivos base

---
+++ fuente/app2/conf/config.php
@@ -0,0 +1,42 @@
+<?php
+    /*variables del sistema, no tocar*/
+    global $estilo;
+    global $titulo;
+    global $inicio;
+    global $ctrlIndex;
+    global $script;
+    global $datos;
+    global $mascara;
+    global $dep;
+    global $traza;
+    global $base;
+    global $ns;
+    /*Titulo Aplicacion*/
+    $titulo=":-- APLICACION 2 --:";
+    /*Modulo de Inicio*/
+    $inicio="";
+    /*ruta inicial*/
+    $base="app2";
+    /*Mi estilo personal afecta toda la aplicacion*/
+    $estilo=array();
+
+    /*Mi javascript personal afecta toda la aplicacion*/
+    $script=array();
+    
+    /* Mostrar o no warning, errors, etc de php*/
+    error_reporting(0);
+    
+    /*COnfiguracion de la conexion a la Base de datos*/
+       
+    $tipo="";
+    $usuario="root";
+    $clave="local";
+    $servidor="localhost";
+    $bd="";
+    
+    /*nombre de session*/
+    $ns="achitun";
+
+    /* habilitar el modo Desarrollo 1; 0 para desactivarlo*/
+    $dep=1;
+?>
---
+++ fuente/app2/vistas/Vindex.php
@@ -0,0 +1,6 @@
+<?php
+function index(){
+	//Codigo aqui
+    echo fTitulo(2,"Estas en la aplicación 2");
+}
+?>
